677c9548fc7a53fa1a065dc2f355e20321a95293
BernardoMendes-afk <rm573355@fiap.com.br>
2026-03-10 17:50:09 -0300

    https://github.com/BernardoMendes-afk/Projetos-Pessoais.git

---
+++ Atividades de sala/Ex. 2.py
@@ -0,0 +1,8 @@
+#Equação
+
+a=3
+b=5
+
+resultado = (2*a)*(3*b)
+
+print(resultado)
---
+++ Atividades de sala/Ex. 5.py
@@ -0,0 +1,7 @@
+#Conversão
+
+num_metros = float(input("Digite o valor, em metros:"))
+
+num_milimetros = num_metros*1000
+
+print("Este valor, em milímetros, é de", num_milimetros,"mm")
