d61720f8698e7ca02bb91ff2bcde037919b479c8
BernardoMendes-afk <rm573355@fiap.com.br>
2026-03-16 20:06:32 -0300

    Ex. 1

--- Aula 3/Ex. 1.py
+++ Aula 3/Ex. 1.py
@@ -1,9 +1,9 @@
 velocidade = float(input("Indique a sua velocidade: "))
 
 if velocidade > 80:
-  multa = (velocidade-80)*5
+    multa = (velocidade-80)*5
     print("Você não é o Dominic Toretto")
-    print("A multa a ser paga é de R$", multa)
+    print(f"A multa a ser paga é de R${multa}")
 
 elif velocidade <= 80:
     print("Muito bem!")
