5560220537d18cb6716265424391c19427ba03e9
BernardoMendes-afk <rm573355@fiap.com.br>
2026-03-23 20:21:01 -0300

    Correção na sintaxe

--- Check-Points/CP - 1.py
+++ Check-Points/CP - 1.py
@@ -5,23 +5,23 @@ spr1 = float(input("Insira a nota do primeiro sprint: "))
 spr2 = float(input("Insira a nota do segundo sprint: "))
 gs = float(input("Insira a nota da Global Solution"))
 
- if cp1 <= cp2 and cp1 <= cp3:
+if cp1 <= cp2 and cp1 <= cp3:
   media = ((cp2 + cp3) + spr1 + spr2)/4 * 0.4 + gs * 0.6
   media_peso = media  * 0.4
   print(f"A sua média desse semestre é {media:.1f}.")
   print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
 
- elif cp2 <= cp1 and cp2 <= cp3:
+elif cp2 <= cp1 and cp2 <= cp3:
     media = ((cp1 + cp3) + spr1 + spr2) / 4 * 0.4 + gs * 0.6
     media_peso = media * 0.4
     print(f"A sua média desse semestre é {media:.1f}.")
     print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
 
- elif cp3 <= cp1 and cp3 <= cp2:
+elif cp3 <= cp1 and cp3 <= cp2:
    media = ((cp1 + cp2) + spr1 + spr2) / 4 * 0.4 + gs * 0.6
    media_peso = media * 0.4
    print(f"A sua média desse semestre é {media:.1f}.")
    print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
 
- else:
+else:
     print("Erro!")
