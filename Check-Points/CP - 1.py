b695b6b4ccadc3e03a254c8fbaa91ba581e84f4e
BernardoMendes-afk <rm573355@fiap.com.br>
2026-04-27 19:50:43 -0300

    Correção

--- Check-Points/CP - 1.py
+++ Check-Points/CP - 1.py
@@ -10,23 +10,15 @@ print("-" * 45)
 if cp1 <= cp2 and cp1 <= cp3:
   media = ((cp2 + cp3) + spr1 + spr2)/4 * 0.4 + gs * 0.6
   media_peso = media  * 0.4
-  print(f"A sua média desse semestre é {media:.1f}.")
-  print("-" * 45)
-  print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
-
 elif cp2 <= cp1 and cp2 <= cp3:
     media = ((cp1 + cp3) + spr1 + spr2) / 4 * 0.4 + gs * 0.6
     media_peso = media * 0.4
-    print(f"A sua média desse semestre é {media:.1f}.")
-    print("-" * 45)
-    print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
-
 elif cp3 <= cp1 and cp3 <= cp2:
    media = ((cp1 + cp2) + spr1 + spr2) / 4 * 0.4 + gs * 0.6
    media_peso = media * 0.4
-   print(f"A sua média desse semestre é {media:.1f}.")
-   print("-" * 45)
-   print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
 
 else:
     print("Erro!")
+print(f"A sua média desse semestre é {media:.1f}.")
+print("-" * 45)
+print(f"A sua média desse semestre, com peso, é {media_peso:.1f}.")
