e022e0c1dc13ce4fd16729d92d5a0f9a03bd3090
BernardoM endes-afk <bernardoromeiramendes@gmail.com>
2026-08-03 20:57:30 -0300

    Aula 6

---
+++ Faculdade/Aula 6/Pratica.py
@@ -0,0 +1,35 @@
+L = [1, 7, 4, 3]
+maximo = L[0]
+for i in L:
+ if i > maximo:
+    maximo = i
+    print(maximo)
+
+
+#for i in range(0,11,2):
+#    print(i)
+#for i in range(3,13,3):
+#    print(i)
+
+#for i in range(2,7):
+#    print(i)
+#print("-"*30)
+#for i in range(5):
+#    print(i)
+
+
+#L = [1, 2, 3, 4, 5]
+#num = int(input("Digite um numero da lista: "))
+#for n in L:
+#    if n == num:
+#        print("Encontrou!")
+#        break
+#else:
+#        print("Não encontrado!")
+
+
+
+
+#Lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#for i in Lista:
+    #print(i)
