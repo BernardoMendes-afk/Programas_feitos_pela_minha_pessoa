0a667fbf63030c016e2e3b1329819a4a66ced180
BernardoMendes-afk <bernardoromeiramendes@gmail.com>
2026-08-17 20:32:39 -0300

    Gustavo Lima e você

--- Faculdade/Aula 6/Pratica.py
+++ Faculdade/Aula 6/Pratica.py
@@ -1,9 +1,56 @@
-L = [1, 7, 4, 3]
-maximo = L[0]
-for i in L:
- if i > maximo:
-    maximo = i
-    print(maximo)
+#alunos = [
+#    {"nome":"Ana", "nota":9.0
+#     },
+#    {
+#        "nome":"Bernardo", "nota":8.0
+#    },
+#    {"nome":"Admilson", "nota":7.0
+#     },
+#]
+
+#print(alunos[0]["nome"])
+#print(alunos[2]["nota"])
+#print(alunos)
+
+
+
+
+
+
+
+#aluno = {'nome':'BerMen', 'idade': 18}
+#print(aluno.values())
+#print(aluno.keys())
+#print(aluno.items())
+#print(aluno.get('nome'))
+
+
+
+
+
+#Dicionario = {"Nome":"Bernardo","RM":573355,"Nota":11}
+#print(Dicionario["Nome"])
+#Dicionario["Status"] = "Aprovado"
+#print(Dicionario)
+
+
+
+#M = [
+#    ["tche","tche rere","tche tche"],
+#    [1,2,3],
+#    ["Gustavo Lima","e","você"]
+#]
+
+#print(M)
+
+
+
+#L = [1, 7, 4, 3]
+#maximo = L[0]
+#for i in L:
+# if i > maximo:
+#    maximo = i
+#    print(maximo)
 
 
 #for i in range(0,11,2):
